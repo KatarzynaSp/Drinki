d8965d32338b7009e4b06a8b01737bc07f28e8be
Katarzyna <katarzyna.panek111@gmail.com>
2019-09-30 19:19:32 +0200

    Dodałam poprawione pliki o nazwach Drink i Menu.

--- src/Drink.java
+++ src/Drink.java
@@ -1,9 +1,9 @@
 public class Drink {
     String name;
-    Double price;
+    double price;
     boolean alcohol;
 
-    Ingredients ingredient1 = new Ingredients();
-    Ingredients ingredient2 = new Ingredients();
-    Ingredients ingredient3 = new Ingredients();
+    Ingredients ingredient1;
+    Ingredients ingredient2;
+    Ingredients ingredient3;
 }
--- src/Menu.java
+++ src/Menu.java
@@ -4,19 +4,25 @@ public class Menu {
         drink.name = "Tequila Sunrise";
         drink.price = 25.50;
         drink.alcohol = true;
-        drink.ingredient1.name = "tequila ";
-        drink.ingredient1.volume = 50 ;
-        drink.ingredient2.name = "orange juice ";
-        drink.ingredient2.volume = 100;
-        drink.ingredient3.name = "grenadina ";
-        drink.ingredient3.volume = 20;
+
+        Ingredients ingredient1 = new Ingredients();
+        ingredient1.name = "tequila ";
+        ingredient1.volume = 50 ;
+
+        Ingredients ingredient2 = new Ingredients();
+        ingredient2.name = "orange juice ";
+        ingredient2.volume = 100;
+
+        Ingredients ingredient3 = new Ingredients();
+        ingredient3.name = "grenadina ";
+        ingredient3.volume = 20;
 
         System.out.println("Name: " + drink.name +"\n"
                 + "Price: " + drink.price + "zł" + "\n"
                 + "Alcohol: " + drink.alcohol +"\n"
-                + "Ingredients: " + drink.ingredient1.name + drink.ingredient1.volume + "ml, "
-                + drink.ingredient2.name + drink.ingredient2.volume + "ml, "
-                + drink.ingredient3.name + drink.ingredient3.volume + "ml, " + "\n"
-                + "Volume: " + (drink.ingredient1.volume + drink.ingredient2.volume + drink.ingredient3.volume) + " ml");
+                + "Ingredients: " + ingredient1.name + ingredient1.volume + "ml, "
+                + ingredient2.name + ingredient2.volume + "ml, "
+                + ingredient3.name + ingredient3.volume + "ml, " + "\n"
+                + "Volume: " + (ingredient1.volume + ingredient2.volume + ingredient3.volume) + " ml");
     }
 }
